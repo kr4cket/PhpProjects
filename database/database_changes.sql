cd748b1e51506b341929875377e73a404f87c0e6
daniil.koreshkov <koreshkov200@mail.ru>
2023-06-19 16:40:02 +0700

    чистая ветка

--- database/database_changes.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE themes CHANGE theme_id id INT;
-ALTER TABLE user_reviews CHANGE message_type theme_id INT, 
-CHANGE user_phoneNumber user_phone_number VARCHAR(255);
-ALTER TABLE user_reviews ADD id INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
-ALTER TABLE user_reviews ADD FOREIGN KEY (theme_id) REFERENCES themes(id) ON DELETE CASCADE;
